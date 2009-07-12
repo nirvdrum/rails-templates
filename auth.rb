af380f7cdae68f4faee09680d2dbda06a6b932cc
Kevin Menard <nirvdrum@gmail.com>
2009-07-11 21:33:15 -0400

    Add authlogic test_case to test_helper.

--- auth.rb
+++ auth.rb
@@ -1,5 +1,8 @@
 load_template "http://github.com/nirvdrum/rails-templates/raw/master/basic.rb"
 
+# Set up authlogic/test_case in test_helper.
+gsub_file 'test/test_helper.rb', /(require 'test_help')/, "\\1\nrequire 'authlogic/test_case'"
+
 # Install all gems
 gem 'authlogic'
 
